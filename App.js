e9e926c954703e2d47c8716a77e33b3e2ae051e3
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2021-09-07 20:54:08 +0000

    redux store provider

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { theme } from "./src/constants/theme";
 import { StatusBar } from "expo-status-bar";
 import AuthStackNavigation from "./src/navigation/AuthStackNavigation";
 import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
+import { store } from "./src/backend/redux/store";
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <AuthStackNavigation />
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <AuthStackNavigation />
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </StoreProvider>
   );
 }
